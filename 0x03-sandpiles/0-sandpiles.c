9d5fba8e2bf5509298c9e16e08d2e7b7883322db
Adam Taylor <tayloradam1999@gmail.com>
2021-12-03 22:26:32 -0600

    Refactored code. Implement tmp_pile[3][3], check_stable(), and _topple()

--- 0x03-sandpiles/0-sandpiles.c
+++ 0x03-sandpiles/0-sandpiles.c
@@ -1,9 +1,9 @@
 #include "sandpiles.h"
 
 /**
- * print_grid - Print 3x3 grid
- * @grid: 3x3 grid
- *
+ * print_grid - prints the grid
+ * @grid: first sandpile
+ * Return: void
  */
 static void print_grid(int grid[3][3])
 {
@@ -22,6 +22,65 @@ static void print_grid(int grid[3][3])
 }
 
 /**
+ * check_stable - check if grid1 is stable
+ * @grid1: First sandpile
+ * Return: 1 if	stable, 0 if not
+ */
+int check_stable(int grid1[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid1[i][j] > 3)
+				return (1);
+		}
+	}
+	return (0);
+}
+
+/**
+ * _topple - Handles toppling grid 1
+ * @grid1: First sandpile
+ * Return: void
+ */
+void _topple(int grid1[3][3])
+{
+	int i, j;
+
+	int tmp_pile[3][3] = {
+		{0, 0, 0},
+		{0, 0, 0},
+		{0, 0, 0}
+	};
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+			tmp_pile[i][j] = grid1[i][j];
+	}
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid1[i][j] > 3)
+				grid1[i][j] -= 4;
+			if ((i - 1) >= 0 && tmp_pile[i - 1][j] > 3)
+				grid1[i][j]++;
+			if ((i + 1) <= 2 && tmp_pile[i + 1][j] > 3)
+				grid1[i][j]++;
+			if ((j - 1) >= 0 && tmp_pile[i][j - 1] > 3)
+				grid1[i][j]++;
+			if ((j + 1) <= 2 && tmp_pile[i][j + 1] > 3)
+				grid1[i][j]++;
+		}
+	}
+}
+
+/**
  * sandpiles_sum - Computes the sum of two sandpiles
  * @grid1: First sandpile
  * @grid2: Second sandpile
@@ -29,44 +88,29 @@ static void print_grid(int grid[3][3])
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	/* When the function finishes, grid1 is stable. */
 	int i, j;
 
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
 		{
-			grid1[i][j] = grid1[i][j] + grid2[i][j];
+			grid1[i][j] += grid2[i][j];
 		}
 	}
-	while (1)
+
+	if (check_stable(grid1))
 	{
-		if (grid1[0][0] > 3 || grid1[0][1] > 3 || grid1[0][2] > 3 ||
-			grid1[1][0] > 3 || grid1[1][1] > 3 || grid1[1][2] > 3 ||
-			grid1[2][0] > 3 || grid1[2][1] > 3 || grid1[2][2] > 3)
+		printf("=\n");
+		print_grid(grid1);
+	}
+
+	while (check_stable(grid1))
+	{
+		_topple(grid1);
+		if (check_stable(grid1))
 		{
 			printf("=\n");
 			print_grid(grid1);
-			for (i = 0; i < 3; i++)
-			{
-				for (j = 0; j < 3; j++)
-				{
-					if (grid1[i][j] > 3)
-					{
-						grid1[i][j] = grid1[i][j] - 4;
-						if (i > 0)
-							grid1[i - 1][j]++;
-						if (i < 2)
-							grid1[i + 1][j]++;
-						if (j > 0)
-							grid1[i][j - 1]++;
-						if (j < 2)
-							grid1[i][j + 1]++;
-					}
-				}
-			}
 		}
-		else
-			break;
 	}
 }
