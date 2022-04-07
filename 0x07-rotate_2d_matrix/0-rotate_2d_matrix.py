9901e705263be321b27f00d670d61cc179a39fce
Adam Taylor <tayloradam1999@gmail.com>
2022-04-07 13:51:53 -0500

    Change to enumerate

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -12,12 +12,12 @@ def rotate_2d_matrix(matrix):
     - @return: nothing - edit in place
     """
     # transpose matrix
-    for i in range(len(matrix)):
+    for i, row in enumerate(matrix):
         for j in range(i, len(matrix)):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-    # reverse each row
+    # reverse rows
     for row in matrix:
         row.reverse()
-
+    
     return matrix
