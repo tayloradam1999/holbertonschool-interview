31c907926bf31e8260f26910898331c0da762f0e
Adam Taylor <tayloradam1999@gmail.com>
2022-04-07 13:43:36 -0500

    PEP8

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,18 +6,18 @@ rotate it 90 degrees clockwise.
 
 
 def rotate_2d_matrix(matrix):
-  """
-  Rotates a 2d matrix 90 degrees clockwise
-  - @matrix: a list of lists
-  - @return: nothing - edit in place
-  """
-  # transpose matrix
-  for i in range(len(matrix)):
-    for j in range(i, len(matrix)):
-      matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    """
+    Rotates a 2d matrix 90 degrees clockwise
+    - @matrix: a list of lists
+    - @return: nothing - edit in place
+    """
+    # transpose matrix
+    for i in range(len(matrix)):
+        for j in range(i, len(matrix)):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-  # reverse each row
-  for row in matrix:
-    row.reverse()
+    # reverse each row
+    for row in matrix:
+        row.reverse()
 
-  return matrix
+    return matrix
