4298f1e98724baabc09c910adb35d931eff1cafe
Adam Taylor <tayloradam1999@gmail.com>
2022-04-28 21:03:44 -0500

    Making Change

---
+++ 0x0F-making_change/0-making_change.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+makeChange function determines the fewest number of coins needed
+to meet a given amount <total>
+"""
+
+
+def makeChange(coins, total):
+    """
+    Prototype: def makeChange(coins, total):
+
+    Args:
+      coins: list of values of coins in your possession
+        (the value of a coin will always be an int > 0)
+        you can assume you have an infinite supply of each coin)
+      total: amount of money you want to make change for
+
+    Return:
+      if total is <= 0: return 0
+      if total cannot be met: return -1
+      if total can be met: return the minimum number of coins needed to meet total
+    """
+    coins.sort(reverse=True)
+    count = 0
+    for coin in coins:
+        if total >= coin:
+            count += total // coin
+            total %= coin
+    return count if total == 0 else -1
---
+++ 0x0F-making_change/README.md
@@ -0,0 +1,3 @@
+# 0x0F-making_change
+
+Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total.
