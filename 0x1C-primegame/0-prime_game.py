fd65addde763c09d9d73f6113ed4fd37cedd7e61
Adam Taylor <tayloradam1999@gmail.com>
2022-05-19 12:42:43 -0500

    Refactor code

--- 0x1C-primegame/0-prime_game.py
+++ 0x1C-primegame/0-prime_game.py
@@ -13,6 +13,7 @@ Assuming Maria always goes first and both players play optimally,
 determine who the winner of each game is.
 """
 
+
 def isWinner(x, nums):
     """
         Returns the winner of the game.
@@ -26,14 +27,30 @@ def isWinner(x, nums):
             Name of player that won most rounds.
             If winner is not determined, returns None.
     """
-    if len(nums) == 0:
-        return None
-    if len(nums) == 1:
+    if x <= 0:
+        raise ValueError("x must be greater than 0")
+    if nums is None or len(nums) == 0:
+        raise ValueError("nums must not be empty")
+
+    # Set the first player to be the first player to move
+    player = 1
+
+    # Loop through each round
+    for i in range(x):
+        # If the player can make a move, remove the number and its multiples
+        if player in nums:
+            nums.remove(player)
+            for j in range(player + player, len(nums) + 1, player):
+                if j in nums:
+                    nums.remove(j)
+
+        # Switch the player
+        player = 1 if player == 2 else 2
+
+    # Return the winner
+    if player == 1:
         return "Maria"
-    if x == 1:
-        return "Maria" if nums[0] == 1 else "Ben"
-    n = len(nums)
-    for i in range(n):
-        if nums[i] in nums[i+1:n]:
-            nums.remove(nums[i])
-    return isWinner(x-1, nums)
+    elif player == 2:
+        return "Ben"
+    else:
+        return None
