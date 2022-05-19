d1c2a0d4b71486729f77a98c2e025f0b35f62503
Adam Taylor <tayloradam1999@gmail.com>
2022-05-19 12:59:47 -0500

    Refactor code

--- 0x1C-primegame/0-prime_game.py
+++ 0x1C-primegame/0-prime_game.py
@@ -27,72 +27,16 @@ def isWinner(x, nums):
             Name of player that won most rounds.
             If winner is not determined, returns None.
     """
-    
-    def isPrime(num):
-        """
-            Returns True if num is a prime number.
-
-            Args:
-                num: integer to check
-
-            Returns:
-                True if num is a prime number, False otherwise
-        """
-        if num < 2:
-            return False
-        if num == 2:
-            return True
-        if num % 2 == 0:
-            return False
-        for i in range(3, int(num ** 0.5) + 1, 2):
-            if num % i == 0:
-                return False
-        return True
-
-    def removeMultiples(num, nums):
-        """
-            Removes all multiples of num from nums.
-
-            Args:
-                num: integer to remove multiples of
-                nums: list of consecutive integers
-
-            Returns:
-                list of consecutive integers without num and its multiples
-        """
-        while num in nums:
-            nums.remove(num)
-        for i in range(num + num, len(nums), num):
-            nums.remove(i)
-        return nums
-
-    def getWinner(nums):
-        """
-            Returns the winner of the game.
-
-            Args:
-                nums: list of consecutive integers
-
-            Returns:
-                Name of player that won most rounds.
-                If winner is not determined, returns None.
-        """
-        if len(nums) == 0:
-            return "Maria"
-        for num in nums:
-            if isPrime(num):
-                nums = removeMultiples(num, nums)
-                return getWinner(nums)
-        return "Ben"
-
-    winners = {"Maria": 0, "Ben": 0}
-    for i in range(x):
-        nums = list(range(1, len(nums) + 1))
-        winner = getWinner(nums)
-        winners[winner] += 1
-    if winners["Maria"] > winners["Ben"]:
-        return "Maria"
-    elif winners["Maria"] < winners["Ben"]:
-        return "Ben"
-    else:
+    if x == 0:
         return None
+    if len(nums) == 0:
+        return None
+    if len(nums) == 1:
+        return "Maria"
+
+    for i in range(x):
+        nums = [n for n in nums if n % nums[0] != 0]
+        if len(nums) == 1:
+            return "Maria"
+        if len(nums) == 0:
+            return "Ben"
